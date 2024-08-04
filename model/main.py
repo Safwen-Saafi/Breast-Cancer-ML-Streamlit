359d4284c7f0381715a4bf1c106b19e863767a5b
Safwen-Saafi <saafi.saf0@gmail.com>
2024-08-04 14:16:42 +0100

    Updated Requirements

--- model/main.py
+++ model/main.py
@@ -3,7 +3,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
-import pickle5 as pickle
+import pickle
 
 #preparing & cleaning the data, 
 def get_clean_data():
