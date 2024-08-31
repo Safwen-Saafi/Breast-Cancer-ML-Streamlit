9d9dceb1f504b84fa00f0d938ece71c85b3755d5
Safwen-Saafi <saafi.saf0@gmail.com>
2024-08-31 17:32:26 +0100

    Adjusted the folder structure for future deployments

--- app/main.py
+++ app/main.py
@@ -3,25 +3,22 @@ import pickle
 import streamlit as st
 import plotly.graph_objects as go
 import numpy as np
-import sys
 from pathlib import Path
 
-
 def get_clean_data():
-  data = pd.read_csv("../data/data.csv")
-  data = data.drop(['Unnamed: 32', 'id', 'fractal_dimension_mean', 'texture_se', 'smoothness_se', 'compactness_se', 'concavity_se', 'symmetry_se', 'fractal_dimension_se', 'fractal_dimension_worst', 'symmetry_mean', 'smoothness_mean'], axis=1)
-  data['diagnosis'] = data['diagnosis'].map({ 'M': 1, 'B': 0 })
-  return data
-
-
-
+    # Adjust the path to point to the root-level data folder
+    data_path = Path(__file__).resolve().parent.parent / "data/data.csv"
+    data = pd.read_csv(data_path)
+    data = data.drop(['Unnamed: 32', 'id', 'fractal_dimension_mean', 'texture_se', 'smoothness_se',
+                      'compactness_se', 'concavity_se', 'symmetry_se', 'fractal_dimension_se',
+                      'fractal_dimension_worst', 'symmetry_mean', 'smoothness_mean'], axis=1)
+    data['diagnosis'] = data['diagnosis'].map({'M': 1, 'B': 0})
+    return data
 
 def add_sidebar():
-  st.sidebar.header("Cell Nuclei Measurements 📏")
-  
-  data = get_clean_data()
-  
-  slider_labels = [
+    st.sidebar.header("Cell Nuclei Measurements 📏")
+    data = get_clean_data()
+    slider_labels = [
         ("Radius (mean)", "radius_mean"),
         ("Texture (mean)", "texture_mean"),
         ("Perimeter (mean)", "perimeter_mean"),
@@ -44,45 +41,37 @@ def add_sidebar():
         ("Symmetry (worst)", "symmetry_worst"),
     ]
 
-  input_dict = {}
-
-  for label, key in slider_labels:
-    input_dict[key] = st.sidebar.slider(
-      label,
-      min_value=float(0),
-      max_value=float(data[key].max()),
-      value=float(data[key].mean())
-    )
-    
-  return input_dict
+    input_dict = {}
+    for label, key in slider_labels:
+        input_dict[key] = st.sidebar.slider(
+            label,
+            min_value=float(0),
+            max_value=float(data[key].max()),
+            value=float(data[key].mean())
+        )
+    return input_dict
 
 def get_scaled_values(input_dict):
-  data = get_clean_data()
-  X = data.drop(['diagnosis'], axis=1)
-  
-  scaled_dict = {}
-  
-  for key, value in input_dict.items():
-    max_val = X[key].max()
-    min_val = X[key].min()
-    scaled_value = (value - min_val) / (max_val - min_val)
-    scaled_dict[key] = scaled_value
-  
-  return scaled_dict
-
+    data = get_clean_data()
+    X = data.drop(['diagnosis'], axis=1)
+    scaled_dict = {}
+    for key, value in input_dict.items():
+        max_val = X[key].max()
+        min_val = X[key].min()
+        scaled_value = (value - min_val) / (max_val - min_val)
+        scaled_dict[key] = scaled_value
+    return scaled_dict
 
 def get_radar_chart(input_data):
-  
-  input_data = get_scaled_values(input_data)
-  
-  categories = ['Radius', 'Texture', 'Perimeter', 'Area', 
-                'Smoothness', 'Compactness', 
-                'Concavity', 'Concave Points',
-                'Symmetry', 'Fractal Dimension']
+    input_data = get_scaled_values(input_data)
+    categories = ['Radius', 'Texture', 'Perimeter', 'Area', 
+                  'Smoothness', 'Compactness', 
+                  'Concavity', 'Concave Points',
+                  'Symmetry', 'Fractal Dimension']
 
-  fig = go.Figure()
+    fig = go.Figure()
 
-  fig.add_trace(go.Scatterpolar(
+    fig.add_trace(go.Scatterpolar(
         r=[
           input_data['radius_mean'], input_data['texture_mean'], input_data['perimeter_mean'],
           input_data['area_mean'], input_data['compactness_mean'],
@@ -91,8 +80,8 @@ def get_radar_chart(input_data):
         theta=categories,
         fill='toself',
         name='Mean Value'
-  ))
-  fig.add_trace(go.Scatterpolar(
+    ))
+    fig.add_trace(go.Scatterpolar(
         r=[
           input_data['radius_se'], input_data['perimeter_se'], input_data['area_se'],
           input_data['concave points_se']
@@ -100,8 +89,8 @@ def get_radar_chart(input_data):
         theta=categories,
         fill='toself',
         name='Standard Error'
-  ))
-  fig.add_trace(go.Scatterpolar(
+    ))
+    fig.add_trace(go.Scatterpolar(
         r=[
           input_data['radius_worst'], input_data['texture_worst'], input_data['perimeter_worst'],
           input_data['area_worst'], input_data['smoothness_worst'], input_data['compactness_worst'],
@@ -110,79 +99,73 @@ def get_radar_chart(input_data):
         theta=categories,
         fill='toself',
         name='Worst Value'
-  ))
+    ))
 
-  fig.update_layout(
-    polar=dict(
-      radialaxis=dict(
-        visible=True,
-        range=[0, 1]
-      )),
-    showlegend=True
-  )
-  
-  return fig
+    fig.update_layout(
+        polar=dict(
+            radialaxis=dict(
+                visible=True,
+                range=[0, 1]
+            )),
+        showlegend=True
+    )
 
-
-
+    return fig
 
 def add_predictions(input_data):
-  model = pickle.load(open("../model/model.pkl", "rb"))
-  scaler = pickle.load(open("../model/scaler.pkl", "rb"))
-  
-  input_array = np.array(list(input_data.values())).reshape(1, -1)
-  
-  input_array_scaled = scaler.transform(input_array)
-  
-  prediction = model.predict(input_array_scaled)
-  
-  st.subheader("Cell cluster prediction")
-  st.write("The cell cluster is:")
-  
-  if prediction[0] == 0:
-    st.write("<span class='diagnosis benign'>Benign</span>", unsafe_allow_html=True)
-  else:
-    st.write("<span class='diagnosis malicious'>Malicious</span>", unsafe_allow_html=True)
+    # Adjust the paths to point to the root-level model folder
+    model_path = Path(__file__).resolve().parent.parent / "model/model.pkl"
+    scaler_path = Path(__file__).resolve().parent.parent / "model/scaler.pkl"
     
-  
-  st.write("Probability of being benign: ", model.predict_proba(input_array_scaled)[0][0])
-  st.write("Probability of being malicious: ", model.predict_proba(input_array_scaled)[0][1])
-  st.write("<span class='warning'>⚠️ IMPORTANT! ⚠️</span>", unsafe_allow_html=True)
-  st.write("<p class='warn'>This app can assist medical professionals in making a diagnosis, but should not be used as a substitute for a professional diagnosis.</p>", unsafe_allow_html=True)
-
-
-
+    # Load model and scaler using absolute paths
+    model = pickle.load(open(model_path, "rb"))
+    scaler = pickle.load(open(scaler_path, "rb"))
+    
+    input_array = np.array(list(input_data.values())).reshape(1, -1)
+    input_array_scaled = scaler.transform(input_array)
+    
+    prediction = model.predict(input_array_scaled)
+    
+    st.subheader("Cell cluster prediction")
+    st.write("The cell cluster is:")
+    
+    if prediction[0] == 0:
+        st.write("<span class='diagnosis benign'>Benign</span>", unsafe_allow_html=True)
+    else:
+        st.write("<span class='diagnosis malicious'>Malicious</span>", unsafe_allow_html=True)
+    
+    st.write("Probability of being benign: ", model.predict_proba(input_array_scaled)[0][0])
+    st.write("Probability of being malicious: ", model.predict_proba(input_array_scaled)[0][1])
+    st.write("<span class='warning'>⚠️ IMPORTANT! ⚠️</span>", unsafe_allow_html=True)
+    st.write("<p class='warn'>This app can assist medical professionals in making a diagnosis, but should not be used as a substitute for a professional diagnosis.</p>", unsafe_allow_html=True)
 
 def main():
     st.set_page_config(
-    page_title="Breast Cancer Predictor",
-    page_icon=":male-doctor:",
-    layout="wide",
-    initial_sidebar_state="expanded"
+        page_title="Breast Cancer Predictor",
+        page_icon=":male-doctor:",
+        layout="wide",
+        initial_sidebar_state="expanded"
     )
 
-    dir = Path(__file__).resolve()
-    sys.path.append(str(dir.parent.parent))
-
-    path_to_model = './style.css'
-      
-    with open(path_to_model) as f:
-      st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+    # Adjust the path to point to the root-level style.css file
+    style_path = Path(__file__).resolve().parent / "style.css"
+    
+    with open(style_path) as f:
+        st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 
     input_data = add_sidebar()
     
     with st.container():
-      st.title("Breast Cancer Predictor 🔬")
-      st.write("Please connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar. ")
+        st.title("Breast Cancer Predictor 🔬")
+        st.write("Please connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar.")
     
-    col1, col2 = st.columns([4,1])
+    col1, col2 = st.columns([4, 1])
     
     with col1:
-      radar_chart = get_radar_chart(input_data)
-      st.plotly_chart(radar_chart)
+        radar_chart = get_radar_chart(input_data)
+        st.plotly_chart(radar_chart)
     with col2:
-      add_predictions(input_data)
+        add_predictions(input_data)
 
 if __name__ == '__main__':
-    main() 
-    
+    main()
